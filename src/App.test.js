0bdcba03fcd1b91621b1297cf8b2199c685d1aa5
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-05 15:29:57 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('The App component', () => {
+  test('renders the header', () => {
+    render(<App />);
+    const header = screen.getByRole('heading');
+    expect(header).toBeInTheDocument();
+  });
+})
